8b0d11ea206ec04a66fae8583f18b4e7fe78d745
SylviaLiu2017 <qinghual@math.uio.no>
2019-09-26 15:26:16 +0200

    update pure .py with plotting function

--- PseudoSample.py
+++ PseudoSample.py
@@ -1,19 +1,19 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[99]:
 
 import numpy as np
 import pandas as pd
 
 
-# In[2]:
+# In[100]:
 
 df = pd.read_csv('./datasets/data_n20_N200.csv',sep=',',header=None)
 df.head()
 
 
-# In[61]:
+# In[101]:
 
 data_arr = np.array(df.loc[1:,1:],dtype=int)
 #data_arr = np.concatenate((data_arr,data_arr))
@@ -21,10 +21,11 @@ data_arr = np.array(df.loc[1:,1:],dtype=int)
 #data_arr = np.concatenate((data_arr,data_arr))
 #data_arr = np.concatenate((data_arr,data_arr))
 #data_arr = np.concatenate((data_arr,data_arr))
-data_arr.shape
+n =data_arr.shape[1]
+N =data_arr.shape[0]
 
 
-# In[62]:
+# In[102]:
 
 def generate_V(centre):
     centre_arr = np.array(centre)
@@ -45,7 +46,7 @@ def generate_V(centre):
     return V_order
 
 
-# In[63]:
+# In[103]:
 
 def rank(ls):
     arr = np.array(ls)
@@ -55,7 +56,7 @@ def rank(ls):
     return ranks
 
 
-# In[64]:
+# In[104]:
 
 def pseudo_likelihood(data,n_samples,alpha):
     data_arr = np.array(data)
@@ -86,19 +87,109 @@ def pseudo_likelihood(data,n_samples,alpha):
     return rho_samples
 
 
-# In[67]:
+# In[114]:
 
+n_samples = 3000
 import time
 start = time.time()
-rho_samples=pseudo_likelihood(data_arr,2000,1)
+rho_samples=pseudo_likelihood(data_arr,n_samples,2.)
 end = time.time()
 #print(end-start)
 print(np.round(end - start,3),"seconds elapsed")
 
 
-# In[68]:
+# In[115]:
+
+import matplotlib
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+
+
+# In[116]:
 
 rho_samples
+
+
+# In[173]:
+
+heatMat=(np.array(list(map(lambda x: np.sum(rho_samples == x,axis=0), (np.arange(1,1+n)))))/n_samples).T[::-1]
+
+
+# In[180]:
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (N, M).
+    row_labels
+        A list or array of length N with the labels for the rows.
+    col_labels
+        A list or array of length M with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear in the bottom.
+    ax.tick_params(top=False, bottom=True,
+                   labeltop=False, labelbottom=True)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+# In[181]:
+
+fig, ax = plt.subplots()
+
+im, cbar = heatmap(heatMat, reversed(np.arange(1,n+1)), (np.arange(1,n+1)), ax=ax,
+                   cmap="coolwarm", cbarlabel= "probability")
+
+
+# In[141]:
+
+#fig, ax = plt.subplots()
+#im = ax.imshow(heatMat)
 
 
 # In[ ]:
